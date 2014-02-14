c30c2ca5687ee221b93d608f0d0ca5525c52b6c8
Jonny Elliott <jonnynelliott@googlemail.com>
2014-02-14 09:10:28 +0100

    Testing Unity with github

---
+++ Assets/keyPress.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+
+public class keyPress : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonUp("Jump"))
+			Debug.Log("We have hit the Space Bar!");
+	
+	}
+}
---
+++ Assets/keyPress.js
@@ -0,0 +1,8 @@
+﻿
+function Update () {
+
+        if(Input.GetButtonUp("Jump")){
+            Debug.Log("We Have Hit the Space Bar!");
+        }
+    }
+
